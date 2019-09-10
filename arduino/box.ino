b9ed00aaa95a1cb8463692641b15ab5455004782
nicolaidavies <nid@trifork.com>
2019-09-10 20:51:58 +0200

    Can read value

--- arduino/box.ino
+++ arduino/box.ino
@@ -1,6 +1,18 @@
 #include "LedControl.h"
 #include "SimpleTimer.h"
 #include <math.h>
+
+
+int encoderPin1 = 2;
+int encoderPin2 = 3;
+
+volatile int lastEncoded = 0;
+volatile long encoderValue = 0;
+
+long lastencoderValue = 0;
+
+int lastMSB = 0;
+int lastLSB = 0;
 /*
  Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
@@ -54,10 +66,24 @@ void setup() {
   lc.clearDisplay(0);
 
   timerId = timer.setInterval(1, countdown);  
+
+
+  // Rotary encoder stuff
+  pinMode(encoderPin1, INPUT);
+  pinMode(encoderPin2, INPUT);
+
+  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
+  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
+
+  //call updateEncoder() when any high/low changed seen
+  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
+  attachInterrupt(0, updateEncoder, CHANGE);
+  attachInterrupt(1, updateEncoder, CHANGE);
 }
 
 void loop() { 
   timer.run();
+  Serial.println(encoderValue);
 }
 
 
@@ -70,4 +96,16 @@ void showOnDisplay(int first, int second, int third, int fourth, int fifth, int 
   lc.setDigit(0, 2, sixth, true);
   lc.setDigit(0, 1, seventh, false);
   lc.setDigit(0, 0, eighth, false);
+}
+
+
+void updateEncoder(){
+  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
+  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
+
+  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number 
+  int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value 
+  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++; 
+  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --; 
+  lastEncoded = encoded; //store this value for next time 
 }
