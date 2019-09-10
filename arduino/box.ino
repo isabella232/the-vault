fa50542e5fdda99ae1ac5b634ca8d39dc81ab66f
nicolaidavies <nid@trifork.com>
2019-09-10 21:01:02 +0200

    Can read left or right from rotary encoder, screens turned off for some reason

--- arduino/box.ino
+++ arduino/box.ino
@@ -1,18 +1,7 @@
 #include "LedControl.h"
 #include "SimpleTimer.h"
+#include "Rotary.h"
 #include <math.h>
-
-
-int encoderPin1 = 2;
-int encoderPin2 = 3;
-
-volatile int lastEncoded = 0;
-volatile long encoderValue = 0;
-
-long lastencoderValue = 0;
-
-int lastMSB = 0;
-int lastLSB = 0;
 /*
  Now we need a LedControl to work with.
  ***** These pin numbers will probably not work with your hardware *****
@@ -21,14 +10,39 @@ int lastLSB = 0;
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
  */
-LedControl lc = LedControl(12,13,10,1);
+LedControl lc = LedControl(12, 13, 10, 1);
 
 // Our timer
 SimpleTimer timer = SimpleTimer();
 int timerId;
-
 // 2 min in milliseconds
 long time = 120000;
+
+// Rotary encoder stuff
+Rotary r1 = Rotary(2, 3);
+
+void setup() {
+  Serial.begin(9600);
+
+  lc.shutdown(0, false); // The MAX72XX is in power-saving mode on startup, we have to do a wakeup call
+  lc.setIntensity(0, 8); /* Set the brightness to a medium values */
+  lc.clearDisplay(0); /* and clear the display */
+
+  // Start our timer here
+  timerId = timer.setInterval(1, countdown);  
+
+  // Listen to changes in rotary encoder
+  r1.begin();
+}
+
+void loop() { 
+  timer.run();
+
+  int result = r1.process();
+  if (result) {
+    Serial.println(result == DIR_CW ? "Right" : "Left");
+  }
+}
 
 void countdown() {
   time = time - 1;
@@ -51,42 +65,6 @@ void countdown() {
   }
 }
 
-
-void setup() {
-  Serial.begin(9600);
-  Serial.println(time);
-  /*
-   The MAX72XX is in power-saving mode on startup,
-   we have to do a wakeup call
-   */
-  lc.shutdown(0,false);
-  /* Set the brightness to a medium values */
-  lc.setIntensity(0,8);
-  /* and clear the display */
-  lc.clearDisplay(0);
-
-  timerId = timer.setInterval(1, countdown);  
-
-
-  // Rotary encoder stuff
-  pinMode(encoderPin1, INPUT);
-  pinMode(encoderPin2, INPUT);
-
-  digitalWrite(encoderPin1, HIGH); //turn pullup resistor on
-  digitalWrite(encoderPin2, HIGH); //turn pullup resistor on
-
-  //call updateEncoder() when any high/low changed seen
-  //on interrupt 0 (pin 2), or interrupt 1 (pin 3)
-  attachInterrupt(0, updateEncoder, CHANGE);
-  attachInterrupt(1, updateEncoder, CHANGE);
-}
-
-void loop() { 
-  timer.run();
-  Serial.println(encoderValue);
-}
-
-
 void showOnDisplay(int first, int second, int third, int fourth, int fifth, int sixth, int seventh, int eighth) {
   lc.setDigit(0, 7, first, false);
   lc.setDigit(0, 6, second, true);
@@ -96,16 +74,4 @@ void showOnDisplay(int first, int second, int third, int fourth, int fifth, int 
   lc.setDigit(0, 2, sixth, true);
   lc.setDigit(0, 1, seventh, false);
   lc.setDigit(0, 0, eighth, false);
-}
-
-
-void updateEncoder(){
-  int MSB = digitalRead(encoderPin1); //MSB = most significant bit
-  int LSB = digitalRead(encoderPin2); //LSB = least significant bit
-
-  int encoded = (MSB << 1) |LSB; //converting the 2 pin value to single number 
-  int sum = (lastEncoded << 2) | encoded; //adding it to the previous encoded value 
-  if(sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue ++; 
-  if(sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue --; 
-  lastEncoded = encoded; //store this value for next time 
 }
